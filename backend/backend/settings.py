2cc27435f91106387c41b0c6bd04a4454a5b6a0d
YianXie <yian.xie@outlook.com>
2025-09-14 13:38:18 +0800

    update settings.py and urls.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -161,10 +161,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "http://0.0.0.0:5173",
-    "http://0.0.0.0:8000",
-    f"http://{env('API_URL')}:5173",
-    f"http://{env('API_URL')}:8000",
     "https://www.lucidgo.org",
 ]
 
