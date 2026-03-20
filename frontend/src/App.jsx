1fa89a4185853b0e8d1e0e75c0a8590876192325
YianXie <yian.xie@outlook.com>
2026-03-20 16:28:19 +0800

    feat: removed blog page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Layout from "./components/layout/Layout";
-import Blogs from "./pages/Blogs";
 import Demo from "./pages/Demo";
 import Docs from "./pages/Docs";
 import Home from "./pages/Home";
@@ -18,8 +17,6 @@ function App() {
                     <Route path="demo/" element={<Demo />} />
                     <Route path="docs/" element={<Docs />} />
                     <Route path="docs/:id/" element={<Docs />} />
-                    <Route path="blogs/" element={<Blogs />} />
-                    <Route path="blogs/:id/" element={<Blogs />} />
                     <Route path="login/" element={<Login />} />
                     <Route path="logout/" element={<Logout />} />
                     <Route path="*" element={<NotFoundPage />} />
--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -2,10 +2,8 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import MenuIcon from "@mui/icons-material/Menu";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Drawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
-import Link from "@mui/material/Link";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
@@ -50,7 +48,6 @@ function Header() {
 
     const navItems = [
         { label: "Docs", path: "/docs" },
-        { label: "Blogs", path: "/blogs" },
         { label: "Demo", path: "/demo" },
         ...(isAuthenticated
             ? [{ label: "Logout", path: "/logout" }]
