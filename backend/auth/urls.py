cd7eb1c7ff525ec07947ce8eb885cfc6984dc1e2
YianXie <yian.xie@outlook.com>
2026-03-30 22:06:33 +0800

    refactor: removed outdated tests and formatted code

--- backend/auth/urls.py
+++ backend/auth/urls.py
@@ -1,6 +1,10 @@
+# fmt: off
+
 from django.urls import path
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
+
+# fmt: on
 
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token-obtain-pair"),
